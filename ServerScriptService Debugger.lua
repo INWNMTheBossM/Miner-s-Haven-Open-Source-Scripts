7b7d58d498f0995114f595a0ccafd90effe7a2ec
INWNMTheBossM <57257103+INWNMTheBossM@users.noreply.github.com>
2020-10-22 00:33:09 +0200

    Create ServerScriptService Debugger.lua

---
+++ ServerScriptService Debugger.lua
@@ -0,0 +1,15 @@
+-- Services
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
+local Items = ReplicatedStorage:WaitForChild('Items')
+
+local MissingPropertyValue = false
+
+for Order, Item in pairs(Items:GetChildren()) do
+    if not Item:FindFirstChild('Cost') or not Item:FindFirstChild('Description') or not Item:FindFirstChild('ItemId') or not Item:FindFirstChild('ItemType') then
+        MissingPropertyValue = true
+    end
+    if MissingPropertyValue == true then
+        warn(Item.Name, 'is missing a required property value!')
+    end
+end
